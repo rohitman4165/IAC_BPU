f00b5d3a58f91d34fc7a1f5e6e87b1d99f106d72
rohitman4165 <rohitnair.infinity@gmail.com>
2017-05-25 11:16:25 +0530

    Hello Azure App Service

---
+++ DjangoApp/DjangoApp/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\rnair\documents\visual studio 2017\Projects\DjangoApp\DjangoApp\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
